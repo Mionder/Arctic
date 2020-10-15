165a360c001b76cf15be585ad576e7155d9b4ae5
Arina <45823655+marsiuser@users.noreply.github.com>
2020-10-15 17:15:48 +0300

    arina main page deskto[

--- js/index.js
+++ js/index.js
@@ -10,6 +10,52 @@ $(document).ready(function(){
     })
     $("#phone__header").on("mouseout", function(){
         $(this).children().css("fill", "#2F97F4");
-    })
-    
+    });
+      $("ul li").hover(function() {
+        $(this)
+          .children("ul")
+          .stop()
+          .slideToggle(500);
+      });   
+      $(".objects").hover(
+        function() {
+          $(this).find(".advantage, .advantage-hover").stop();
+          $(this).find(".advantage").slideUp();
+          $(this).find(".advantage-hover").slideDown();
+        },
+        function() {
+          $(this).find(".advantage, .advantage-hover").stop();
+          $(this).find(".advantage").slideDown();
+          $(this).find(".advantage-hover").slideUp();
+      
+        }
+      );
+      $(".header-bottom__scroll").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });    
+    $(".owl-carousel").owlCarousel({
+        nav: false,
+        dots: false,
+        items:5,
+        margin: 20,
+        navText: ["", ""],
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        }
+    });
 });
